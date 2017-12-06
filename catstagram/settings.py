2630ce8a7338da0238aa1e40150eefaf9eaec476
aHunsader <ahunsader@s-169-232-111-178.resnet.ucla.edu>
2017-12-06 02:57:52 -0800

    v1.6

--- catstagram/settings.py
+++ catstagram/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['fierce-garden-66987.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['cat-stagram.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
